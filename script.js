048239861df465c5fdcd2f28b1f2643c5433bd23
Yagon Don <you@example.com>
2025-11-08 13:02:08 +0200

    Finished code,Added a circle circumfierence and laukums

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ console.log("Hello world!")
 const inputNumber = document.getElementById("mainInput")
 const firstButton = document.getElementById("mainButton")
 const mainOutput = document.getElementById("Output")
+const SecondOutput = document.getElementById("OutputLaukums")
 
-mainButton.addEventListener("click",)
+mainButton.addEventListener("click", handelClicks)
 
+function handelClicks(){
+    let radiuss = mainInput.value
+    mainOutput.innerText = 2 * 3.14 * mainInput.value
+    SecondOutput.innerText = 3.14 * mainInput.value * mainInput.value
+}
 
