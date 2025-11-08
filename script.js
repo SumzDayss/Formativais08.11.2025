d5fef4af12c79fa40b5b04a026fd8596cbf698db
Yagon Don <you@example.com>
2025-11-08 12:55:54 +0200

    Added Elements to js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello world!")
+
+const inputNumber = document.getElementById("mainInput")
+const firstButton = document.getElementById("mainButton")
+const mainOutput = document.getElementById("Output")
+
+mainButton.addEventListener("click",)
+
+
